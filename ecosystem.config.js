9d59b0d0f907c62ca2130986cde94d5df9e27956
MIKHAIL MARTSINYUK <martsinyuk.pl@gmail.com>
2024-06-16 14:50:03 +0300

    try fx

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    apps: [
-      {
-        name: 'ledovod-ai',
-        script: 'venv/bin/gunicorn',
-        args: 'app.main:app --bind localhost:3000 --workers 3',
-        interpreter: 'none',
-        env: {
-          FLASK_ENV: 'production',
-        },
+  apps: [
+    {
+      name: "ledovod-ai",
+      script: "venv/bin/gunicorn",
+      args: "app.main:app --bind localhost:3000 --workers 3 --worker-class uvicorn.workers.UvicornWorker",
+      interpreter: "none",
+      env: {
+        FLASK_ENV: "production",
       },
-    ],
-  };
-  
+    },
+  ],
+};
